799abbc7ead4b4c6bcc6c090f3d0db60d1602ae3
Sam Kniffer <rtmonster@null.net>
2015-10-02 08:48:11 -0600

    Delete menu.php

--- menu.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo '<a href="http://harpnet.mooo.com">Main Site</a> | 
-<a href="http://zk12.mooo.com">Content Server</a> | 
-<a href="http://chan.harpnet.mooo.com">HarpChan</a> | 
-<a href="http://yellow.qq.my">Yellow's Insane Asylum</a>';
-?>
