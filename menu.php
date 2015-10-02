95cb94a7d94d4c78ae8d6f296aab55b9009a1994
Sam Kniffer <rtmonster@null.net>
2015-10-02 08:47:15 -0600

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,6 @@
+<?php
+echo '<a href="http://harpnet.mooo.com">Main Site</a> | 
+<a href="http://zk12.mooo.com">Content Server</a> | 
+<a href="http://chan.harpnet.mooo.com">HarpChan</a> | 
+<a href="http://yellow.qq.my">Yellow's Insane Asylum</a>';
+?>
